c6698ab8f96e4c5f4d5a9afe2a858b4ec9ab7a17
tihonel <stihonin@yandex.ru>
2024-09-17 19:47:08 +0400

    init base project

---
+++ src/test/java/ru/neoflex/vacation_pay_service/VacationPayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.neoflex.vacation_pay_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationPayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
